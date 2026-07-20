92fe3f0ab21207a8156e311ca42338a3915049ca
Matheus <matheus.resteves@gmail.com>
2026-07-20 12:08:10 -0300

    :sparkles: feat: status com dados do banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-    const result = await database.query("SELECT 1 + 1;");
-    console.log(result.rows);
-    response.status(200).json({ message: "Ta funcionando chefe", status: 200 });
+    const updatedAt = new Date().toISOString();
+    const databaseVersion = await database.query("SHOW server_version;");
+    const maxConnections = await database.query("SHOW max_connections;");
+
+    const databaseName = process.env.POSTGRES_DB;
+    const openedConnections = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName],
+    });
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                database_version: databaseVersion.rows[0].server_version,
+                max_connections: parseInt(
+                    maxConnections.rows[0].max_connections,
+                ),
+                opened_connections: openedConnections.rows[0].count,
+            },
+        },
+    });
 }
 
 export default status;
