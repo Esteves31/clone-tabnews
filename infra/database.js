3fce51828afe422f369459d1c63025197ff062c6
Matheus <matheus.resteves@gmail.com>
2026-07-20 12:50:21 -0300

    :wrench: chore: loga credenciais de conexão

--- infra/database.js
+++ infra/database.js
@@ -9,8 +9,15 @@ async function query(queryObject) {
         database: process.env.POSTGRES_DB,
     });
 
-    await client.connect();
+    console.log("Credenciais do banco de dados:", {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+    });
+
     try {
+        await client.connect();
         const result = await client.query(queryObject);
         return result;
     } catch (error) {
