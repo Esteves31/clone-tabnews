c02bb94617b62f131020d3953880df8839ab77d8
Matheus <matheus.resteves@gmail.com>
2026-07-23 13:51:47 -0300

    :recycle: refactor: migrations usam dbClient

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,12 @@
 import { join } from "node:path";
 import migrationRunner from "node-pg-migrate";
+import database from "infra/database.js";
 
 async function migrations(request, response) {
+    const dbClient = await database.getNewClient();
+
     const defaultMigrationConfig = {
-        databaseUrl: process.env.DATABASE_URL,
+        dbClient: dbClient,
         dir: join("infra", "migrations"),
         direction: "up",
         dryRun: true,
@@ -13,6 +16,7 @@ async function migrations(request, response) {
 
     if (request.method === "GET") {
         const pendingMigrations = await migrationRunner(defaultMigrationConfig);
+        dbClient.end();
         return response.status(200).json(pendingMigrations);
     }
 
@@ -21,6 +25,8 @@ async function migrations(request, response) {
             ...defaultMigrationConfig,
             dryRun: false,
         });
+
+        dbClient.end();
 
         if (migratedMigrations.length > 0) {
             return response.status(201).json(migratedMigrations);
