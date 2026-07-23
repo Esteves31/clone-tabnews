f9251e10922e0620325ced8dd2ad9112587ea038
Matheus <matheus.resteves@gmail.com>
2026-07-23 11:47:51 -0300

    :test_tube: test: limpeza de banco e novas asserções

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+    await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
+
 test("POST / api/v1/migrations should return 200 OK", async () => {
     const response = await fetch("http://localhost:3000/api/v1/migrations", {
         method: "POST",
@@ -5,8 +13,17 @@ test("POST / api/v1/migrations should return 200 OK", async () => {
             "Content-Type": "application/json",
         },
     });
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(201);
 
     const responseBody = await response.json();
     expect(Array.isArray(responseBody)).toBe(true);
+
+    const responseAfterPost = await fetch(
+        "http://localhost:3000/api/v1/migrations",
+    );
+    expect(responseAfterPost.status).toBe(200);
+
+    const responseBodyAfterPost = await responseAfterPost.json();
+    expect(Array.isArray(responseBodyAfterPost)).toBe(true);
+    expect(responseBodyAfterPost.length).toBe(0);
 });
